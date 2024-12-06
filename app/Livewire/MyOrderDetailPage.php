2e2c50449944a06ec6767b908b900bf9e55dba2b
buildwebpro <buildwebpro@gmail.com>
2024-12-06 12:57:39 +0700

    thevisionoptic
    ecommerce project for thevision

---
+++ app/Livewire/MyOrderDetailPage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('Order Detail')]
+class MyOrderDetailPage extends Component {
+
+	public $order_id;
+
+	public function mount($order_id) {
+		$this->order_id = $order_id;
+
+		$order = Order::where('id', $this->order_id)->first();
+
+		if (!$order) {
+			abort(404);
+		}
+
+		if ($order->user_id != auth()->id()) {
+			abort(403);
+		}
+	}
+
+	public function render() {
+		$order_items = OrderItem::with('product')->where('order_id', $this->order_id)->get();
+		$address = Address::where('order_id', $this->order_id)->first();
+		$order = Order::where('id', $this->order_id)->first();
+		return view('livewire.my-order-detail-page', [
+			'order_items' => $order_items,
+			'address' => $address,
+			'order' => $order
+		]);
+	}
+}
